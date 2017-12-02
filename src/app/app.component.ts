cb5d3cd07b2140713ab016f1ef1f904776d3968c
Edwin Fuentes <efuentesamin@gmail.com>
2017-12-02 11:39:50 -0500

    Authentication added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,43 +2,113 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Events, AlertController, NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-
-  rootPage: any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+    @ViewChild(Nav) nav: Nav;
+    rootPage: any = HomePage;
+    public menuItems = [
+        {label: 'Inicio', page: HomePage, icon: 'md-time'},
+        {label: 'Lista', page: LoginPage, icon: 'md-list'},
+        {label: 'Salir', page: null, icon: 'md-exit'},
     ];
 
-  }
+    constructor(
+        private platform: Platform,
+        private statusBar: StatusBar,
+        private splashScreen: SplashScreen,
+        private storage: Storage,
+        private events: Events,
+        private alertController: AlertController,
+        private authService: AuthServiceProvider
+    ) {
+        this.initializeApp();
+        this.events.subscribe('user:logged-in', () => {
+            // When user loggs in take him to home page
+            this.rootPage = HomePage;
+        });
+        this.events.subscribe('user:logged-out', () => {
+            // When user loggs out take him to login page
+            this.rootPage = LoginPage;
+        });
+        this.events.subscribe('http:error', (error) => {
+            this.handleError(error);
+        });
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+        // Ask whether user is logged in
+        this.authService.isLoggedIn(loggedIn => {
+            if (!loggedIn) {
+                // this.rootPage = LoginPage;
+            }
+        });
+    }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
+
+    handleMenuItem(item) {
+        //* There are menu items for pages and menu items for actions
+        if (item.page) {
+            this.nav.setRoot(item.page);
+        } else {
+            //* Handle actions
+            switch (item.label) {
+                case 'Salir': {
+                    this.authService.logout();
+                }
+            }
+        }
+    }
+
+    handleError(error) {
+        console.log(error);
+        // Default error message on http error
+        let message = 'Ocurrió un error!';
+
+        // Whether http error has a message
+        if (error._body) {
+            let response;
+            try {
+                response = JSON.parse(error._body);
+            } catch(e) {
+            }
+
+            if (response) {
+                message = response.message || 'Ocurrió un error!';
+            }
+        }
+        
+        switch (error.status) {
+            case 401: {
+                //* Unauthorized response. Remove token and send user to login
+                this.storage.remove('nequi_token');
+                this.events.publish('user:logged-out');
+            }
+            default: {
+                // For other error codes present the alert message
+                const alert = this.alertController.create({
+                    title: 'Error',
+                    subTitle: message,
+                    buttons: ['OK']
+                });
+                alert.present();
+            }
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HttpServiceProvider } from '../providers/http-service/http-service';
 import { ErrorHandlerProvider } from '../providers/error-handler/error-handler';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
 @NgModule({
     declarations: [
         MyApp,
         HomePage,
-        ListPage
+        LoginPage
     ],
     imports: [
         BrowserModule,
+        HttpModule,
+        HttpClientModule,
+        IonicStorageModule.forRoot(),
         IonicModule.forRoot(MyApp),
     ],
     bootstrap: [IonicApp],
     entryComponents: [
         MyApp,
         HomePage,
-        ListPage
+        LoginPage
     ],
     providers: [
         StatusBar,
         SplashScreen,
         { provide: ErrorHandler, useClass: IonicErrorHandler },
         HttpServiceProvider,
-        ErrorHandlerProvider
+        ErrorHandlerProvider,
+        AuthServiceProvider
     ]
 })
 export class AppModule { }
