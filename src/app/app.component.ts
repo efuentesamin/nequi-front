8a4579d25037953ab43b5f244a3c01a63ecfa39e
Jesus Herazo <developer@fuimonos.co>
2017-12-03 06:49:54 -0500

    Updating Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,8 +25,8 @@ export class MyApp {
     public menuItems = [
         { label: 'Inicio', page: HomePage, icon: 'md-home' },
         { label: 'Clases', page: ClassesPage, icon: 'md-contacts' },
-        { label: 'Logros', page: AchievementsPage, icon: 'md-star' },
-        { label: 'Ranking', page: RankingPage, icon: 'md-star' },
+        { label: 'Logros', page: AchievementsPage, icon: 'md-trophy' },
+        { label: 'Ranking', page: RankingPage, icon: 'md-people' },
         { label: 'Salir', page: null, icon: 'md-exit' },
     ];
 
